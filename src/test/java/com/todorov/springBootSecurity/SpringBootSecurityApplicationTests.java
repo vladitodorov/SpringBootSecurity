79d32c7b4d9eacf8fc54c83122267407912c97e4
vladitodorov <vladitodorov81@gmail.com>
2021-02-22 08:46:42 +0200

    Initial update.

---
+++ src/test/java/com/todorov/springBootSecurity/SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todorov.springBootSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
