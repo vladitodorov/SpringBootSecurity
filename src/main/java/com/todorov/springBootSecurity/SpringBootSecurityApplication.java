0c0ac508719ac1c2d74ac8c78b65d983d20821cb
vladitodorov <vladitodorov81@gmail.com>
2021-02-23 09:52:04 +0200

    Enable HTTPS/SSL in Spring Boot.

--- src/main/java/com/todorov/springBootSecurity/SpringBootSecurityApplication.java
+++ src/main/java/com/todorov/springBootSecurity/SpringBootSecurityApplication.java
@@ -1,13 +1,50 @@
 package com.todorov.springBootSecurity;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootSecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSecurityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityApplication.class, args);
+    }
 
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        //Enable SSL Traffic
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        //Add HTTP to HTTPS redirect
+        tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+
+        return tomcat;
+    }
+
+    //Redirect from HTTP to HTTPS
+    private Connector httpToHttpsRedirectConnector() {
+        Connector connector = new Connector();
+        connector.setScheme("http");
+        connector.setPort(80);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
+    }
 }
+
